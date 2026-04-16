514ea803b89c702390d630d71c2ca900427e3576
julius <Julius.Braitinger@kavisio.de>
2026-04-16 09:57:37 +0200

    create files for critic method

---
+++ pipeline/critic/critic.py
@@ -0,0 +1,5 @@
+#Pyhton file that implements the CRITIC method for choosing the best datasets.
+#  It uses the AHP weights as a starting point and then adjusts
+#  them based on the variability and conflict of the criteria.
+
+#TODO implement the CRITIC method 
---
+++ pipeline/critic/scroe.py
@@ -0,0 +1,6 @@
+# Python script that implements the actual score system to later use in the CRITIC method. 
+# It will take the results from the hard rules and assign scores to each candidate dataset
+#  based on how well they meet the criteria. The scores will then be used to rank the datasets 
+# and select the best one for the task at hand.
+
+#TODO implement scoring system for the datasets based on the results of the hard rules.
