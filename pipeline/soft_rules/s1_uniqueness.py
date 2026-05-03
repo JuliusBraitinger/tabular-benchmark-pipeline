d261f2ecbad3e786e434f8fb78a222a138e343ad
julius <Julius.Braitinger@kavisio.de>
2026-05-03 11:32:17 +0200

    fix score function s1

--- pipeline/soft_rules/s1_uniqueness.py
+++ pipeline/soft_rules/s1_uniqueness.py
@@ -7,9 +7,9 @@ import pandas as pd
 from pipeline.soft_rules.base import SoftRuleResult
 
 def score(dataset):
-    stdv = dataset.std()
-    mean = dataset.mean()
-    skew = dataset.skew()
-    kurt = dataset.kurtosis()
-    fingerprint = pd.concat([mean, stdv, skew, kurt], axis=1)
+    stdv = dataset.X.std()
+    mean = dataset.X.mean()
+    skew = dataset.X.skew()
+    kurt = dataset.X.kurtosis()
+    fingerprint = pd.concat([mean, stdv, skew, kurt], axis=0)
     return SoftRuleResult(rule="S1", score=1.0, details={"fingerprint": fingerprint}) #score is placeholder for now
