0fa7e7264d2706cc245a0519d7307d01f6f265db
julius <Julius.Braitinger@kavisio.de>
2026-05-06 18:42:42 +0200

    defined score function in S5

--- pipeline/soft_rules/s5_batch_effects.py
+++ pipeline/soft_rules/s5_batch_effects.py
@@ -2,3 +2,8 @@
 # DROPPED: no reliable automated detection method
 
 #TODO revisit if a good detection method is found
+import pandas as pd
+from pipeline.soft_rules.base import SoftRuleResult
+
+def score(dataset, pool=None):
+    return SoftRuleResult(rule="S5", score=1.0, details={})
