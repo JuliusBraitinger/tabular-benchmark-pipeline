c6cc6a2358e7f0cec5ec1cac1eeae1236b6c8c76
julius <Julius.Braitinger@kavisio.de>
2026-05-22 18:30:06 +0200

    duplication via hashing s1

--- pipeline/soft_rules/s2_iid.py
+++ pipeline/soft_rules/s2_iid.py
@@ -4,6 +4,27 @@
 #TODO implement score()
 
 from pipeline.soft_rules.base import SoftRuleResult
+import pandas as pd
 
+
+def hash_rows(dataset):
+    x= dataset.X
+    y= dataset.y
+    combined = pd.concat([x, y], axis=1)
+    hashed = pd.util.hash_pandas_object(combined, index=False)
+    return hashed 
+
+def find_duplicates(dataset):
+    hashed = hash_rows(dataset)
+    counter = hashed.value_counts()
+    duplicates_groups = counter[counter > 1]
+    n_rows = len(hashed)
+    n_duplidates_rows = duplicates_groups.sum() #every row that belongs to any duplicate group
+    return {
+        "n_rows": n_rows,
+        "n_duplicate_rows": n_duplidates_rows,
+        "n_duplicate_groups": int(len(n_duplidates_rows)),
+        "duplicate_fraction": n_duplidates_rows / n_rows
+    }
 def score(dataset, pool=None):
     return SoftRuleResult(rule="S2", score=1.0, details={})
